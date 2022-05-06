1ff68b3fb5c90d76bbf74c1b1ac4f82a94ed6282
Sean Campbell <scampbell@mutley.net.au>
2022-05-06 21:04:23 +1000

    Initial commit

---
+++ TestFixture/WebHost.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+
+namespace TestFixture
+{
+    public class TestFixtureWebHost
+    {
+        public (IWebHost, string) StartHost(Assembly assembly, IConfigurationRoot config = null)
+        {
+            var _config = config ?? new ConfigurationBuilder().AddJsonFile("hostsettings.json", optional: true).Build();
+            var _url = FetchNextAvailableUrl();
+
+            var builder = WebHost.CreateDefaultBuilder()
+                            .CaptureStartupErrors(true)
+                            .UseUrls(_url)
+                            .UseKestrel( options => {})
+                            .UseConfiguration(_config)
+                            .UseStartup(assembly.FullName);
+
+            return (builder.Build(), _url);
+        }
+
+        private string FetchNextAvailableUrl() => $"http://*:{FetchNextAvailablePort()}";
+
+        private int FetchNextAvailablePort()
+        {
+            using( var socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Bind(new IPEndPoint(IPAddress.IPv6Loopback, 0));
+                return ((IPEndPoint)socket.LocalEndPoint).Port;
+            }
+        }
+    }
+}
---
+++ host/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using System.Reflection;
+
+namespace hosting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new TestFixture.TestFixtureWebHost();
+            var (webhost, url) = host.StartHost(typeof(webapi.Program).Assembly);
+            Console.WriteLine($"Now listening on {url}");
+
+            webhost.Run();
+
+            Console.WriteLine("Started host");
+            Console.Read();
+
+           // CreateHostBuilder(args).Build().Run();
+
+        }
+
+        //public static IWebHostBuilder CreateHostBuilder(string[] args)
+        //{
+
+        //    var config = new ConfigurationBuilder()
+        //                    .AddJsonFile("hostsettings.json", optional: true)
+        //                    .AddCommandLine(args)                            
+        //                    .Build();
+
+        //    return WebHost.CreateDefaultBuilder(args)
+        //                    .CaptureStartupErrors(true)
+        //                    .UseUrls("http://*:5000;https://*:5001")
+        //                    .UseConfiguration(config)
+        //                    .UseKestrel( options => {})
+        //                    .UseStartup(typeof(webapi.Program).Assembly.FullName);
+        //}
+    }
+}
